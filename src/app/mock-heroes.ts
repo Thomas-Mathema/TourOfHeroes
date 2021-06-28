72faaf9996e96de91490098a1ab7f63a9775ae8b
Thomas Tran <thomas.tran@mathema.de>
2021-06-28 11:01:01 +0200

    Initialize commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Thor' },
+  { id: 12, name: 'Black Widow' },
+  { id: 13, name: 'Hulk' },
+  { id: 14, name: 'Iron Man' },
+  { id: 15, name: 'Spiderman' },
+  { id: 16, name: 'Dr. Strange' },
+  { id: 17, name: 'Hawkeye' },
+  { id: 18, name: 'Captain Marvel' },
+  { id: 19, name: 'Black Panther' },
+  { id: 20, name: 'Scarlet Witch' }
+];
+
